b2e8f0857f0a6773db7ee35170ed43aae1bfd795
Testing_data <45229703+picturedewa@users.noreply.github.com>
2020-03-24 15:11:39 +0700

    upload apimodel

---
+++ application/models/Apimodel.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class ApiModel extends CI_Model
+{
+  public $soh = 'so_h';
+  
+
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->database();
+  }
+
+  public function getAllomset($idtgl){
+    if($idtgl){
+        $time = strtotime($idtgl);
+        $bulan = date('m', $time);
+        $tahun = date('Y', $time);
+      }else{
+        $bulan = date('m');
+        $tahun = date('Y');
+      }
+    $this->db->select('A.noso,A.tgl,A.grandtotal,A.ttlhpp,A.grandtotal-A.ttlhpp AS rl');
+    $this->db->from("{$this->soh} A");
+    $this->db->where('YEAR(A.tgl)', $tahun);
+    $this->db->where('MONTH(A.tgl)', $bulan);
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+
+
+}
