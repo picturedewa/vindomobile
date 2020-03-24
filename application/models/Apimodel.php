2ca2fd2d9427c036f9d1153567c0447b52b5894a
picturedewa <picturedewa@gmail.com>
2020-03-24 15:32:57 +0700

    update lagi

--- application/models/Apimodel.php
+++ application/models/Apimodel.php
@@ -1,39 +1,3 @@
-<<<<<<< HEAD:application/models/Apimodel.php
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
-class ApiModel extends CI_Model
-{
-  public $soh = 'so_h';
-  
-
-  public function __construct()
-  {
-    parent::__construct();
-    $this->load->database();
-  }
-
-  public function getAllomset($idtgl){
-    if($idtgl){
-        $time = strtotime($idtgl);
-        $bulan = date('m', $time);
-        $tahun=date('Y',$time);
-      }else{
-        $bulan=date('m');
-        $tahun=date('Y');
-      }
-    $this->db->select('A.noso,A.tgl,A.grandtotal,A.ttlhpp,A.grandtotal-A.ttlhpp AS rl');
-    $this->db->from("{$this->soh} A");
-    $this->db->where('YEAR(A.tgl)', $tahun);
-    $this->db->where('MONTH(A.tgl)', $bulan);
-    $query = $this->db->get();
-    return $query->result();
-  }
-
-
-
-}
-=======
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
@@ -68,4 +32,3 @@ class ApiModel extends CI_Model
 
 
 }
->>>>>>> cb7060d828b0f8a4ebdcf921d72430e8b029ddf9:application/models/Apimodel.php
