eb0cc670323ecf1fffc82a5d4d8a97eac2db91d8
picturedewa <picturedewa@gmail.com>
2020-03-25 12:49:16 +0700

    edit api openedit-6

--- application/models/Apimodel.php
+++ application/models/Apimodel.php
@@ -103,52 +103,52 @@ class ApiModel extends CI_Model
 
   public function updateoepnedit($idno,$idmodule,$idevent){
     print_r($idevent);
-    if ($idevent) = "1" {
-        $data = array( 
-          'ocedit'      => $_POST[$idevent] ,  
-        );
-    }
-    if ($idevent) = "2" {
-        $data = array( 
-          'ocdel'      => $_POST[$idevent] ,  
-        );
-    }
+  //   if ($idevent) = "1" {
+  //       $data = array( 
+  //         'ocedit'      => $_POST[$idevent] ,  
+  //       );
+  //   }
+  //   if ($idevent) = "2" {
+  //       $data = array( 
+  //         'ocdel'      => $_POST[$idevent] ,  
+  //       );
+  //   }
     
 
-    if($idmodule) ="inv" {
-      $this->db->where('noso', $idno);
-      return $this->db->update($this->soh,$data);
-    }
+  //   if($idmodule) ="inv" {
+  //     $this->db->where('noso', $idno);
+  //     return $this->db->update($this->soh,$data);
+  //   }
     
-    if($idmodule) ="po" {
-      $this->db->where('nopo', $idno);
-      return $this->db->update($this->poh,$data);
-    }
+  //   if($idmodule) ="po" {
+  //     $this->db->where('nopo', $idno);
+  //     return $this->db->update($this->poh,$data);
+  //   }
 
-    if($idmodule) ="retjual" {
-      $this->db->where('noret', $idno);
-      return $this->db->update($this->retjualh,$data);
-    }
+  //   if($idmodule) ="retjual" {
+  //     $this->db->where('noret', $idno);
+  //     return $this->db->update($this->retjualh,$data);
+  //   }
 
-    if($idmodule) ="retbeli" {
-      $this->db->where('noret', $idno);
-      return $this->db->update($this->retbelih,$data);
-    }
+  //   if($idmodule) ="retbeli" {
+  //     $this->db->where('noret', $idno);
+  //     return $this->db->update($this->retbelih,$data);
+  //   }
 
-    if($idmodule) ="lnsspl" {
-      $this->db->where('nobyr', $idno);
-      return $this->db->update($this->lnsspl,$data);
-    }
+  //   if($idmodule) ="lnsspl" {
+  //     $this->db->where('nobyr', $idno);
+  //     return $this->db->update($this->lnsspl,$data);
+  //   }
 
-    if($idmodule) ="spl" {
-      $this->db->where('kodecst', $idno);
-      return $this->db->update($this->spl,$data);
-    }
+  //   if($idmodule) ="spl" {
+  //     $this->db->where('kodecst', $idno);
+  //     return $this->db->update($this->spl,$data);
+  //   }
 
-    if($idmodule) ="prod" {
-      $this->db->where('kodepro', $idno);
-      return $this->db->update($this->prod,$data);
-    }
+  //   if($idmodule) ="prod" {
+  //     $this->db->where('kodepro', $idno);
+  //     return $this->db->update($this->prod,$data);
+  //   }
     
-  }
+  // }
 }
